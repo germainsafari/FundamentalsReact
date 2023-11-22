9d5246cf1cb51bd118dcd99b75ca94feb4e3f6fc
Safari Germain <pzx115866@student.wsb.poznan.pl>
2023-11-22 16:24:36 +0100

    somechanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App () {
+
+  const [color, setColor] = useState("")
+  const [newItem, setNewItem] = useState("damn")
+
+  const handleclick = () => {
+    setColor('green')
+  }
   return (
-    <div className="">
+    <>
 
       <form action="">
-        <label htmlFor=""></label>
-      
+        <div className="new-item-form">
+          <label htmlFor="item">New item</label>
+          <input value={newItem} type="text" id="item"/>
+        </div>
+        <button className="btn" onClick={() => setColor("darkblue")}>Add</button>
+        
       </form>
+      <h1>HomeComing</h1>
+      <div>
+        <ul className="list">
+          <li>
+            <label >
+              <input type="checkbox" /> Christmas
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type="checkbox" />
+              Easter
+            </label>
+          </li>
+          <li>
+            <label >
+              <input type="checkbox" /> Christmas
+            </label>
+          </li>
 
+        </ul>
+      </div>
 
-    </div>
+    </>
+    
   )
 }
